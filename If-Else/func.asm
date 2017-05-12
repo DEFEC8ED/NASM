c5754eabde77c8599d93f3f64c779c9fa3356174
zpx <DEFEC8ED@users.noreply.github.com>
2017-05-12 21:06:36 +0200

    Create func.asm

---
+++ If-Else/func.asm
@@ -0,0 +1,58 @@
+one:
+	mov eax, 4
+	mov ebx, STDOUT
+	mov ecx, msg1
+	mov edx, msg1_len
+	int 0x80
+
+	jmp gameover
+
+two:
+	mov eax, 4
+	mov ebx, STDOUT
+	mov ecx, msg2
+	mov edx, msg2_len
+	int 0x80
+
+	jmp gameover
+
+three:
+	mov eax, 4
+	mov ebx, STDOUT
+	mov ecx, msg3
+	mov edx, msg3_len
+	int 0x80
+
+	jmp gameover
+
+four:
+	mov eax, 4
+	mov ebx, STDOUT
+	mov ecx, msg4
+	mov edx, msg4_len
+	int 0x80
+
+	jmp gameover
+
+greater:
+	mov eax, 4
+	mov ebx, STDOUT
+	mov ecx, greatter
+	mov edx, greatL
+	int 0x80
+
+	jmp gameover
+
+less:
+	mov eax, 4
+	mov ebx, STDOUT
+	mov ecx, msg_less
+	mov edx, msg_less_len
+	int 0x80
+
+	jmp gameover
+
+gameover:
+	mov eax, 1
+	mov ebx, 0
+	int 0x80
